724e74b4971e508eef195d71b58d6686c1f33bd2
Moor21 <moor21chik@gmail.com>
2026-08-04 15:51:45 +0000

    Working on parse_args and execv

--- process.c
+++ process.c
@@ -12,17 +12,32 @@
 typedef struct{
     int fd[2];
     char *command;
-    char *args[];
+    int argc;
+    char **args;
+
 } Params;
 void parse_args(int argc, char **argv, Params *params){
+    if(argc == 1){
+        params->argc = 0;
+        params->args = malloc(sizeof(char *));
+        params->args[0] = NULL;
+        return;
+    }
 	printf("\nargc: %i\n", argc);
  	for(int i = 0; i < argc; i++){
 	 printf("\nargv[%d]: %s\n",i, argv[i]);
 	}	
-       for (int i = 0; i < argc-1; i++){
-	params->args[i] = argv[i+1];
+       params->argc = argc;
+       params->args = malloc((params->argc+2) * sizeof(char *));
+       if(params->args == NULL){
+           perror("Memory allocation failed");
+           exit(1);
        }
-       for(int i = 0; i < argc -1;i++){
+       for (int i = 0; i < argc-1; i++){
+        	params->args[i] = argv[i+1];
+       }
+       params->args[argc-1] = NULL;
+       for(int i = 0; i < argc;i++){
 	       printf("\nparams->args[%d]: %s\n", i, params->args[i]);
        }
 }
@@ -40,10 +55,36 @@ int nesquick(void *arg){
         printf("\nError: Mounting failed\n");
         exit(1);
    }
-    char const *args[] = { "/bin/bash", "-c","ls","-la", NULL };
-    execv( (char const *)args[0], (char * const *)args);
+   //char *args[params->argc+2];
+   //args[0] = "/bin/bash";
+   //args[1] = "-c";
+   // for (int i = 0; i < params->argc; i++){
+   //     args[i+2] = params->args[i];
+   // }
+   // for (int i = 0; i < params->argc+2; i++){
+    //    printf("\nargs[%i]: %s\n",i, args[i]);
+    //}
+  //  char const *args[] = { "/bin/bash", "-c","ls","-la", NULL };
+  //  execv( (char const *)args[0], (char * const *)args);
+   // perror("Execution failed");
+ //   exit(EXIT_FAILURE);
+    if(params->argc == 0){
+        printf("\nThere are no arguments passed!\n");
+        exit(1);
+    }
+    char *args[params->argc+2];
+    printf("\nparams->argc+2: %d\n", params->argc+2);
+    args[0] = "/bin/bash";
+    args[1] = "-c";
+    for(int i = 2; i < params->argc+2; i++){
+        args[i] = params->args[i-2];
+    }
+    for (int i =0; i < params->argc+2; i++){
+        printf("\nargs[%d]: %s\n", i , args[i]);
+    }
+    execv((char *const)args[0], (char *const *)args);
     perror("Execution failed");
-    exit(EXIT_FAILURE); 
+    exit(EXIT_FAILURE);    
     return 1;
 }
 int main(int argc, char **argv){
